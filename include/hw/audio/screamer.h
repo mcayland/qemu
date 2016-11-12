2a7651d73d9344705bfbee1676837a76b91223b5
Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
2016-11-12 16:39:17 +0000

    screamer: some basic reg work, plus basic DMA wire-up

--- include/hw/audio/screamer.h
+++ include/hw/audio/screamer.h
@@ -27,10 +27,13 @@
 
 #include "qemu/osdep.h"
 #include "hw/sysbus.h"
+#include "hw/ppc/mac_dbdma.h"
 #include "qemu/audio.h"
 
 #define TYPE_SCREAMER "screamer"
 OBJECT_DECLARE_SIMPLE_TYPE(ScreamerState, SCREAMER)
+
+#define SCREAMER_BUFFER_SIZE 0x4000
 
 struct ScreamerState {
     /*< private >*/
@@ -39,8 +42,20 @@ struct ScreamerState {
     /*< public >*/
     MemoryRegion mem;
     qemu_irq irq;
+    void *dbdma;
+    qemu_irq dma_tx_irq;
 
     AudioBackend *be;
+    SWVoiceOut *voice;
+    uint8_t  buf[SCREAMER_BUFFER_SIZE];
+    uint32_t bpos;
+    uint32_t ppos;
+    DBDMA_io io;
+
+    uint32_t regs[6];
+    uint32_t codec_ctrl_regs[7];
 };
+
+void macio_screamer_register_dma(ScreamerState *s, void *dbdma, int txchannel);
 
 #endif
